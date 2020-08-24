17ca628176134cc87cc8795163cd2fc77ddb3c45
Magesh Kuppan <tkmagesh77@gmail.com>
2020-08-24 15:45:23 +0530

    memoization

---
+++ 01-intro/memoize.js
@@ -0,0 +1,14 @@
+function memoize(fn){
+    var cache = {};
+    return function(){
+        var key = JSON.stringify(arguments);
+        if (typeof cache[key] === 'undefined')
+            cache[key] = fn.apply(this,arguments);
+        return cache[key];
+    }
+}
+undefined
+var add = memoize(function(x,y){
+    console.log('processing ', x , ' and ', y);
+    return x + y;
+})
