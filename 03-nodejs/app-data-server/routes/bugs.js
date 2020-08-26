9a1b862dc80c0f28534918903f1f7c014b242945
Magesh Kuppan <tkmagesh77@gmail.com>
2020-08-26 16:49:59 +0530

    Exposing GET requests for bugs api

--- 03-nodejs/app-data-server/routes/bugs.js
+++ 03-nodejs/app-data-server/routes/bugs.js
@@ -1,8 +1,25 @@
 var express = require('express'),
     router = express.Router();
 
+var bugsList = [
+    { id : 1, name : 'Server communication failure', isClosed : false, createdAt : new Date() },
+    { id : 2, name: 'Data integrity checks failed', isClosed: true, createdAt: new Date() },
+];
+
+
 router.get('/', (req, res, next) => {
-    res.send('All the bugs will be displayed here');
+    res.json(bugsList);
 });
+
+router.get('/:id', (req, res, next) => {
+    const requestedBugId = parseInt(req.params.id),
+        requestedBug = bugsList.find(bug => bug.id === requestedBugId);
+    if (requestedBug){
+        res.json(requestedBug);
+    } else {
+        res.status(404).end();
+    }
+});
+
 
 module.exports = router;
