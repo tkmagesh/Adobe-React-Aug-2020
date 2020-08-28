26f62491f8c350ea6cd097aac0b6d85829e20c9f
Magesh Kuppan <tkmagesh77@gmail.com>
2020-08-28 10:03:47 +0530

    SalaryCalculatorInput as function component

--- 04-react/simple-app/src/salaryCalculator/SalaryCalculatorInput.jsx
+++ 04-react/simple-app/src/salaryCalculator/SalaryCalculatorInput.jsx
@@ -1,19 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class SalaryCalculatorInput extends Component {
-    render() {
-        const { labelText, onValueChange, type = "number", ...restProps } = this.props;
-        return (
-            <div className="field">
-                <label htmlFor="">{labelText}</label>
-                <input
-                    type={type}
-                    onChange={evt => onValueChange(evt.target.valueAsNumber)}
-                    {...restProps}
-                />
-            </div>
-        )
-    }
-}
+const SalaryCalculatorInput = ({ labelText, onValueChange, type = "number", ...restProps }) => (
+    <div className="field">
+        <label htmlFor="">{labelText}</label>
+        <input
+            type={type}
+            onChange={evt => onValueChange(evt.target.valueAsNumber)}
+            {...restProps}
+        />
+    </div>
+);
 
 export default SalaryCalculatorInput;
