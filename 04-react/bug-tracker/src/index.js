317c8609598a1ba06d69fa3bc8b3a20c0eedfc95
Magesh Kuppan <tkmagesh77@gmail.com>
2020-08-29 10:04:24 +0530

    loading bugs from local

--- 04-react/bug-tracker/src/index.js
+++ 04-react/bug-tracker/src/index.js
@@ -2,19 +2,76 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import {useSelector, useDispatch, Provider} from 'react-redux';
+import { createStore, combineReducers } from 'redux';
 
-import axios from 'axios';
-window['axios'] = axios;
+
+
+function bugsReducer(currentState = [], action){
+  switch (action.type) {
+    case 'INIT_BUGS':
+      return action.payload;
+    default:
+      return currentState;
+  }
+}
+
+const rootReducer = combineReducers({
+  bugsData : bugsReducer
+});
+
+const appStore = createStore(rootReducer);
+
+function getLocalBugs() {
+  const bugs = [
+    { id: 1, name: 'Bug - 1', isClosed: false },
+    { id: 2, name: 'Bug - 2', isClosed: true },
+    { id: 3, name: 'Bug - 3', isClosed: false },
+  ];
+  return bugs;
+}
+
+/* 
+function getRemoteBugs() {
+  return axios.get('http://localhost:3030/bugs')
+    .then(response => response.data);
+} 
+*/
+
+var bugActions = {
+  load(){
+    const bugs = getLocalBugs();
+    const action = { type : 'INIT_BUGS', payload: bugs };
+    return action;
+  }
+}
 
 const BugTracker = () => {
+  const dispatch = useDispatch();
+  const bugs = useSelector(state => state.bugsData);
+
+  const onLoadClick = () => {
+    const loadAction = bugActions.load();
+    dispatch(loadAction);
+  }
+
+  const bugItems = bugs.map((bug, index) => (
+    <li key={index}>
+      {bug.isClosed ? (<div className="bugname closed">{bug.name}</div>) : (<div className="bugname">{bug.name}</div>) }
+      <div className="datetime">{bug.createdAt}</div>
+      <input type="button" value="Remove" />
+    </li>
+  ));
+
   return (
     <div>
       <h3>Bugs</h3>
+      <input type="button" value="Load Bugs" onClick={onLoadClick} />
       <hr/>
       <section className="stats">
         <span className="closed">0</span>
         <span> / </span>
-        <span>0</span>
+        <span>{bugs.length}</span>
       </section>
       <section className="edit">
         <label htmlFor="">Bug Name :</label>
@@ -23,16 +80,7 @@ const BugTracker = () => {
       </section>
       <section className="list">
         <ol id="bugList">
-          <li>
-            <div className="bugname">Bug - 1</div>
-            <div className="datetime">[Created At]</div>
-            <input type="button" value="Remove" />
-          </li>
-          <li>
-            <div className="bugname closed">Bug - 2</div>
-            <div className="datetime">[Created At]</div>
-            <input type="button" value="Remove" />
-          </li>
+         {bugItems}
         </ol>
         <input type="button" value="Remove Closed" />
       </section>
@@ -42,9 +90,11 @@ const BugTracker = () => {
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Bug Tracker</h1>
-    <hr/>
-    <BugTracker></BugTracker>
+    <Provider store={appStore}>
+      <h1>Bug Tracker</h1>
+      <hr/>
+      <BugTracker></BugTracker>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
